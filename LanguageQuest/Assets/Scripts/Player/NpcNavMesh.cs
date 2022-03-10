7971c576d3be703975a8022b075176764b1258c6
Jake-Bon <73080033+Jake-Bon@users.noreply.github.com>
2022-03-10 16:41:39 -0500

    NPC Roaming fully functioning
    Spheres act as destinations for NPCs
    NPCs can have any number of destinations with custom wait times

--- LanguageQuest/Assets/Scripts/Player/NpcNavMesh.cs
+++ LanguageQuest/Assets/Scripts/Player/NpcNavMesh.cs
@@ -4,29 +4,45 @@ using UnityEngine;
 
 public class NpcNavMesh : MonoBehaviour
 {
-    public Transform dest;
+    public Transform[] dest;
     public Transform npc;
+    public float minTimeout;
+    public float maxTimeout;
 
     public float colDistance = 0.1f;
     
     private UnityEngine.AI.NavMeshAgent mesh;
-    int minTimeout = 5;
-    int maxTimeout = 15;
+    float timeout = 0;
+    int destListLength = 0;
+    int targetedDest = 0;
+    float colTime;
+    bool collided = false;
     
 
-    Vector3 positionChange = new Vector3(0, 0, 1);
     // Start is called before the first frame update
     private void Start()
     {
+        destListLength = dest.Length;
         mesh = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        if(minTimeout>maxTimeout){
+            minTimeout = maxTimeout;
+        }
+        targetedDest = Random.Range(0,dest.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        mesh.destination = dest.position;
-        if((npc.position - dest.position).sqrMagnitude < .5f){
-            dest.position+=positionChange;
+        mesh.destination = dest[targetedDest].position;
+        if(collided){
+            if(Time.time>=colTime+timeout){
+                targetedDest = Random.Range(0,dest.Length);
+                collided = false;
+            }
+        }else if((npc.position - dest[targetedDest].position).sqrMagnitude < .5f){
+            colTime = Time.time;
+            timeout = Random.Range(minTimeout,maxTimeout);
+            collided = true;
         }
     }
 }
