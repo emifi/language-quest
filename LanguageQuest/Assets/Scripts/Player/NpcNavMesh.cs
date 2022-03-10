cb202f5c1949bcaea60859ba1ade3a9910eb33a8
Jake-Bon <73080033+Jake-Bon@users.noreply.github.com>
2022-03-10 17:35:42 -0500

    NPC Scripting added - Roam and Script NPCs under same script

--- LanguageQuest/Assets/Scripts/Player/NpcNavMesh.cs
+++ LanguageQuest/Assets/Scripts/Player/NpcNavMesh.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class NpcNavMesh : MonoBehaviour
 {
+    public enum NpcType {Roam,Proximity};
+
     public Transform[] dest;
-    public Transform npc;
+    public GameObject npc;
+    public Transform player;
     public float minTimeout;
     public float maxTimeout;
+    public NpcType walkType;
 
-    public float colDistance = 0.1f;
+    public float colDistance = 0.5f;
     
     private UnityEngine.AI.NavMeshAgent mesh;
     float timeout = 0;
@@ -17,6 +21,7 @@ public class NpcNavMesh : MonoBehaviour
     int targetedDest = 0;
     float colTime;
     bool collided = false;
+    bool complete = false;
     
 
     // Start is called before the first frame update
@@ -27,19 +32,35 @@ public class NpcNavMesh : MonoBehaviour
         if(minTimeout>maxTimeout){
             minTimeout = maxTimeout;
         }
-        targetedDest = Random.Range(0,dest.Length);
+        if(walkType==NpcType.Roam){
+            targetedDest = Random.Range(0,dest.Length);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        mesh.destination = dest[targetedDest].position;
-        if(collided){
-            if(Time.time>=colTime+timeout){
-                targetedDest = Random.Range(0,dest.Length);
+        if(!complete){ //Hidden away from complete proximity NPC
+            mesh.destination = dest[targetedDest].position;
+        }
+        
+        if(collided){ //If collision, react to timeout or nearby player
+            if(walkType==NpcType.Roam&&Time.time>=colTime+timeout){
+                targetedDest = Random.Range(0,dest.Length); //assign new random spot to roam
                 collided = false;
             }
-        }else if((npc.position - dest[targetedDest].position).sqrMagnitude < .5f){
+            if(walkType==NpcType.Proximity&&Time.time>=colTime+timeout&&(npc.transform.position - player.position).sqrMagnitude < colDistance){ //Nearby player detection
+                targetedDest++;
+                if(targetedDest<dest.Length){ //If more locations exist, start navigation
+                    collided = false;
+                }else{ //Otherwise, disallow any more roaming.
+                    MeshRenderer renderer = npc.GetComponent<MeshRenderer>();
+                    renderer.material.color = Color.red;
+                    collided = false;
+                    complete = true;
+                }
+            }
+        }else if((!complete&&(npc.transform.position - dest[targetedDest].position).sqrMagnitude < colDistance)){ //Upon collision, take time and determine timeout
             colTime = Time.time;
             timeout = Random.Range(minTimeout,maxTimeout);
             collided = true;
