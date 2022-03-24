d6ded019e9dd45b9a9e909c7db230f1954f3b4b3
emifi <eaf53@pitt.edu>
2022-03-24 14:41:50 -0400

    Added animation to removal

--- LanguageQuest/Assets/Scripts/Objective System/ObjectiveSystem.cs
+++ LanguageQuest/Assets/Scripts/Objective System/ObjectiveSystem.cs
@@ -8,22 +8,30 @@ public class ObjectiveSystem : MonoBehaviour
     public List<Objective> objectives = new List<Objective>();
     public GameObject objectiveUI;
     public GameObject panelUI;
+
+    Objective example;
     // Start is called before the first frame update
     void Start()
     {
         foreach (Objective obj in objectives) {
             addObjective(obj);
         }
+        example = new Objective(ObjectiveType.Collect, "Pick up bucket", Resources.Load<PickupObject>("Items/Bucket"), 0, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P)) {
-            addObjective(new Objective(ObjectiveType.Collect, "Pick up bucket", Resources.Load<PickupObject>("Items/Bucket"), 0, 1));
+            addObjective(example);
+        }
+        if (Input.GetKeyDown(KeyCode.O)) {
+            removeAllObjectives();
         }
     }
 
+    // Adds an Objective to the UI and to the current objectives container
+    // The container object is only public for editor convenience! Never add directly to it!
     public void addObjective(Objective objective) {
         if (!objectives.Contains(objective)) objectives.Add(objective);
         objective.panelUI = Instantiate(panelUI);
@@ -37,42 +45,65 @@ public class ObjectiveSystem : MonoBehaviour
         objective.panelUI.transform.SetParent(objectiveUI.transform);
     }
 
-    public void removeAllObjectives() {
-        foreach (Objective objective in objectives) {
-            Destroy(objective.panelUI);
-        }
-        objectives.Clear();
+    // Starts coroutine to fade out and remove the specified objective
+    public void removeObjective(Objective objective) {
+        StartCoroutine(fadeAndRemove(0f, objective));
     }
 
+    IEnumerator fadeAndRemove(float t, Objective objective) {
+        yield return new WaitForSeconds(2.0f);
+        while (t < 2.0) {
+            objective.panelUI.GetComponent<TMP_Text>().color = Color.Lerp(Color.white, new Color(1f, 1f, 1f, 0f), t/2.0f);
+            t += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(objective.panelUI);
+        objectives.Remove(objective);
+    }
+
+    public void removeAllObjectives() {
+        foreach (Objective objective in objectives) {
+            removeObjective(objective);
+        }
+    }
+
+    // Fires from PlayerInteraction.cs when a pickup interaction is detected
     public void pickupEvent(PickupObject item) {
         foreach (Objective objective in objectives) {
             if (objective.status == ObjectiveStatus.Complete) continue;
             if (objective.type != ObjectiveType.Collect) continue;
             if (objective.item == item) {
                 objective.current += 1;
-                if (objective.current == objective.required) objective.status = ObjectiveStatus.Complete;
+                if (objective.current == objective.required) {
+                    objective.status = ObjectiveStatus.Complete;
+                    StartCoroutine(fadeAndRemove(0.0f, objective));
+                }
                 updateUI(objective);
             }
         }
     }
 
+    // Fires from PlayerInteraction.cs when an interaction with a trigger item is detected
     public void triggerEvent(TriggerObject trigger) {
         foreach (Objective objective in objectives) {
             if (objective.status == ObjectiveStatus.Complete) continue;
             if (objective.type != ObjectiveType.Trigger) continue;
             if (objective.trigger == trigger) {
                 objective.status = ObjectiveStatus.Complete;
+                StartCoroutine(fadeAndRemove(0.0f, objective));
                 updateUI(objective);
             }
         }
     }
 
+    // Fires from PlayerInteraction.cs when the player enters a collision trigger
     public void destinationEvent(DestinationObject destination) {
         foreach (Objective objective in objectives) {
             if (objective.status == ObjectiveStatus.Complete) continue;
             if (objective.type != ObjectiveType.MoveTo) continue;
             if (objective.destination == destination) {
                 objective.status = ObjectiveStatus.Complete;
+                StartCoroutine(fadeAndRemove(0.0f, objective));
                 updateUI(objective);
             }
         }
