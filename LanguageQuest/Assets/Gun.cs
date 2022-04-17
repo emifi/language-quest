4d534825dd301bf73c3967b198bfdfc7de535c94
emifi <eaf53@pitt.edu>
2022-04-17 15:57:58 -0400

    Bullet trail + unity effect pack

--- LanguageQuest/Assets/Gun.cs
+++ LanguageQuest/Assets/Gun.cs
@@ -9,6 +9,7 @@ public class Gun : MonoBehaviour
     bool isFiring = false;
     float fireTime = 0.0f;
     public float range;
+    public TrailRenderer bulletTrail;
     float fireDelay = 0.1f;
     // Start is called before the first frame update
     void Start()
@@ -22,22 +23,22 @@ public class Gun : MonoBehaviour
     void Update()
     {
         if (!isFiring && Input.GetMouseButtonDown(0)) {
-            Debug.Log("BANG");
             muzzleFlash.SetActive(true);
             isFiring = true;
             RaycastHit hit;
             Ray ray = new Ray(playerCamera.position, playerCamera.forward);
             // If raycast detects an interactable
             if (Physics.Raycast(ray, out hit, range)) {
+                TrailRenderer trail = Instantiate(bulletTrail, muzzleFlash.transform.position, Quaternion.identity);
+                StartCoroutine(createTrail(trail, hit.point));
+                // Layer 10 is the wildlife layer (for the deer)
                 if (hit.collider.gameObject.layer == 10) {
                     Animator animator = hit.collider.GetComponentInParent<Animator>();
                     animator.SetBool("isDead", true);
                     ItemInteractable interactable = hit.collider.gameObject.AddComponent<ItemInteractable>();
                     interactable.item = Resources.Load<ItemObject>("Items/Caribou");
-                    Debug.Log("Hit!");
                 }
                 else {
-                    Debug.Log("Miss!");
                 }
             }
         } else if (isFiring) {
@@ -50,8 +51,19 @@ public class Gun : MonoBehaviour
         }
     }
 
-    IEnumerator flash() {
-        yield return 0;
+    IEnumerator createTrail(TrailRenderer trail, Vector3 target) {
+        float bulletSpeed = 75.0f;
+        float distance = Vector3.Distance(trail.transform.position, target);
+        float time = 0.0f;
+        float endTime = distance/bulletSpeed;
+        Vector3 startPosition = trail.transform.position;
+        //trail.time = endTime;
+        while (time < endTime) {
+            trail.transform.position = Vector3.Lerp(startPosition, target, time/endTime);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(trail.gameObject);
     }
 
     GameObject findRoot(string name, GameObject current) {
