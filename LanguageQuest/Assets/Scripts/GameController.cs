f32086b746c91abca71632e3ff8559d6ef442dab
emifi <eaf53@pitt.edu>
2022-04-18 21:05:23 -0400

    LoadScene gamecontroller

--- LanguageQuest/Assets/Scripts/GameController.cs
+++ LanguageQuest/Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameController : MonoBehaviour
 {
@@ -136,6 +138,10 @@ public class ObjectiveDialogueGroup
         if (pair.npc != null) {
             pair.npc.setDialoguePointer(pair.ptr);
         }
+    }
+
+    public void LoadScene(string name) {
+        SceneManager.LoadScene(name);
     }
 }
 
