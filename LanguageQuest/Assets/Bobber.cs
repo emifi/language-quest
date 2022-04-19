64c0d47409cd0a95ee7f44800ab868ed692296e6
emifi <eaf53@pitt.edu>
2022-04-19 17:09:10 -0400

    Fishing minigame bones

---
+++ LanguageQuest/Assets/Bobber.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bobber : MonoBehaviour
+{
+    FishingRod rod;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rod = GameObject.Find("Fishing_Rod").GetComponent<FishingRod>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Water")) {
+            rod.hitWater();
+        }
+    }
+}
