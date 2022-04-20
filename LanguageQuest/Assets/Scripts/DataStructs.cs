88ccbafb6287eb8f0fa8a99af3c8f2e414e0b271
Jake-Bon <73080033+Jake-Bon@users.noreply.github.com>
2022-04-19 22:57:13 -0400

    Scene-Specific Objectives

--- LanguageQuest/Assets/Scripts/DataStructs.cs
+++ LanguageQuest/Assets/Scripts/DataStructs.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public static class DataStructs
 {
+    public static List<ObjectiveDialogueGroup>[] objectiveDialogueGroups = new List<ObjectiveDialogueGroup>[SceneManager.sceneCountInBuildSettings];
+    public static List<ObjectiveDialogueGroup>[] hiddenObjectiveDialogueGroups = new List<ObjectiveDialogueGroup>[SceneManager.sceneCountInBuildSettings];
     public static InventoryObject inventory = Resources.Load<InventoryObject>("Inventory/Player Inventory");
     public static NotebookObject notebook = Resources.Load<NotebookObject>("Inventory/Player Notebook");
     public static string playerName = "Player";
