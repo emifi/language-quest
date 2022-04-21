e0372568e38881f20d4e4645a9f17ac77c6185a3
Jake-Bon <73080033+Jake-Bon@users.noreply.github.com>
2022-04-21 01:26:24 -0400

    NPC soft copies

--- LanguageQuest/Assets/Scripts/DataStructs.cs
+++ LanguageQuest/Assets/Scripts/DataStructs.cs
@@ -7,6 +7,7 @@ public static class DataStructs
 {
     public static List<ObjectiveDialogueGroup>[] objectiveDialogueGroups = new List<ObjectiveDialogueGroup>[SceneManager.sceneCountInBuildSettings];
     public static List<ObjectiveDialogueGroup>[] hiddenObjectiveDialogueGroups = new List<ObjectiveDialogueGroup>[SceneManager.sceneCountInBuildSettings];
+    public static Dictionary<string,NPCPointers>[] NPCS = new Dictionary<string,NPCPointers>[SceneManager.sceneCountInBuildSettings];
     public static InventoryObject inventory = Resources.Load<InventoryObject>("Inventory/Player Inventory");
     public static NotebookObject notebook = Resources.Load<NotebookObject>("Inventory/Player Notebook");
     public static string playerName = "Player";
--- LanguageQuest/Assets/Scripts/NPCs/NpcNavMesh.cs
+++ LanguageQuest/Assets/Scripts/NPCs/NpcNavMesh.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Subtegral.DialogueSystem.DataContainers;
+using UnityEngine.SceneManagement;
 
 public class NpcNavMesh : MonoBehaviour
 {
@@ -29,6 +30,8 @@ public class NpcNavMesh : MonoBehaviour
     float colTime;
     bool collided = false;
     bool complete = false;
+
+    NPCPointers np;
     
 
     // Start is called before the first frame update
@@ -51,6 +54,25 @@ public class NpcNavMesh : MonoBehaviour
         }
         player = GameObject.Find("First Person Player").GetComponent<Transform>();
         animator = gameObject.GetComponentInChildren<Animator>();
+
+        Dictionary<string,NPCPointers> npcDict = DataStructs.NPCS[SceneManager.GetActiveScene().buildIndex-1];
+        if(npcDict==null){
+            DataStructs.NPCS[SceneManager.GetActiveScene().buildIndex-1] = new Dictionary<string,NPCPointers>();
+            npcDict = DataStructs.NPCS[SceneManager.GetActiveScene().buildIndex-1];
+            npcDict.Add(this.gameObject.name,new NPCPointers(dialoguePtr,destinationsPtr,targetedDest));
+            np = npcDict[this.gameObject.name];
+        }else{
+            if(npcDict.ContainsKey(this.gameObject.name)){
+                np = npcDict[this.gameObject.name];
+                dialoguePtr = np.dialoguePtr;
+                targetedDest = np.targetedDest;
+                destinationsPtr = np.destinationsPtr;
+            }else{
+                npcDict.Add(this.gameObject.name,new NPCPointers(dialoguePtr,destinationsPtr,targetedDest));
+                np = npcDict[this.gameObject.name];
+            }
+        }
+        
     }
 
     // Update is called once per frame
@@ -73,10 +95,12 @@ public class NpcNavMesh : MonoBehaviour
         if(collided){ //If collision, react to timeout or nearby player
             if((walkType==NpcType.Roam || walkType == NpcType.Reactive)&&Time.time>=colTime+timeout){
                 targetedDest = Random.Range(0,destinationGroups[destinationsPtr].dests.Count); //assign new random spot to roam
+                np.targetedDest = targetedDest;
                 collided = false;
             }
             if(walkType==NpcType.Proximity&&Time.time>=colTime+timeout&&(npc.transform.position - player.position).sqrMagnitude < colDistance){ //Nearby player detection
                 targetedDest++;
+                np.targetedDest = targetedDest;
                 if(targetedDest<destinationGroups[destinationsPtr].dests.Count || walkType == NpcType.Reactive){ //If more locations exist, start navigation
                     collided = false;
                 }else{ //Otherwise, disallow any more roaming.
@@ -89,6 +113,7 @@ public class NpcNavMesh : MonoBehaviour
             else if (walkType == NpcType.Reactive && (npc.transform.position - player.position).sqrMagnitude < colDistance) {
                 Debug.Log("HOLY FUCK THAT GUYS GOTTA GUN");
                 targetedDest = Random.Range(0,destinationGroups[destinationsPtr].dests.Count);
+                np.targetedDest = targetedDest;
                 collided = false;
             }
         }else if((!complete&&(npc.transform.position - destinationGroups[destinationsPtr].dests[targetedDest].position).sqrMagnitude < colDistance)){ //Upon collision, take time and determine timeout
@@ -119,19 +144,33 @@ public class NpcNavMesh : MonoBehaviour
     }
 
     public DialogueContainer getCurrDialogue(){
+        Debug.Log(dialoguePtr);
         return dialogue[dialoguePtr];
     }
 
     public void setDialoguePointer(int ptr) {
         dialoguePtr = ptr;
+        np.dialoguePtr = ptr;
     }
 
     public void setDestinationPointer(int ptr){
         destinationsPtr = ptr;
+        np.destinationsPtr = ptr;
     }
 }
 
 [System.Serializable]
 public class DestinationArray{
     public List<Transform> dests = new List<Transform>();
+}
+
+public class NPCPointers{
+    public int dialoguePtr;
+    public int targetedDest;
+    public int destinationsPtr;
+    public NPCPointers(int diap, int td, int desp){
+        dialoguePtr = diap;
+        targetedDest = td;
+        destinationsPtr = desp;
+    }
 }
