835fc1ce7fe964fe4efd6d8bc07ed22d9e4ae760
Jake-Bon <73080033+Jake-Bon@users.noreply.github.com>
2022-04-21 20:57:46 -0400

    Fixed Syntax

--- LanguageQuest/Assets/Scripts/UI/DialogueUI.cs
+++ LanguageQuest/Assets/Scripts/UI/DialogueUI.cs
@@ -224,16 +224,16 @@ public class DialogueUI : MonoBehaviour
 
         //Objective Parsing - ADDQUEST{quest0,quest1...questN,dialoguePTR}/ADDQUEST{quest0,quest1...questN,dialoguePTR}
         string cpy = "" + fullText;
-        do while(fullText!=cpy){
+        do{
             fullText = objectiveParse(fullText,currNPC);
-        }
+        }while(fullText!=cpy);
 
         //Objective Parsing -   ADDQUEST++{quest0,quest1...questN,NPC0:dialoguePTR...NPC1:dialoguePTR}
         //                      ADDQUESTS++{quest0,quest1...questN,NPC0:dialoguePTR...NPC1:dialoguePTR}
-        string cpy = "" + fullText;
-        do while(fullText!=cpy){
+        cpy = "" + fullText;
+        do{
         fullText = objectiveParseMultNPC(fullText); //++{Collect-jak-3,MoveTo-NW,Trigger-Book,NPC (1):2, NPC (2):2}
-        }
+        }while(fullText!=cpy);
 
         //Term Parsing (no print variation) - ADDTERM{item0,item1...itemN}/ADDTERMS{item0,item1...itemN}
         fullText = addTermParse(fullText);
